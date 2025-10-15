428d267742bde6251504bd23eae6214ebce5778e
AleksandraChaban <blueberry04p@gmail.com>
2025-10-15 12:04:50 +0300

    Доделать класс Bank

--- src/Program.java
+++ src/Program.java
@@ -4,6 +4,8 @@ public class Program{
 
     public static void main (String args[]){
 
-        System.out.println("Hello METANIT.COM!");
+        System.out.println("Введите ID: ");
+
+        System.out.println("Основное меню \n1: провести балас счета \n2: снять со счета \n3: положить на счет \n4: выйти \nВведите пункт меню: ");
     }
 }
